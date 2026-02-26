c3d023a55635d008fe8aa2f952f3236bdf021f0d
ekrmabd <ekramhossainekram28@gmail.com>
2026-02-26 10:18:55 +0600

    leetcode 121 problem solved

---
+++ leetcode_121.js
@@ -0,0 +1,19 @@
+//121. Best Time to Buy and Sell Stock
+
+var maxProfit = function(prices) {
+    let buy = prices[0];
+    let maxProfit = 0;
+    for(let i = 1; i < prices.length; i++)
+    {
+        if(prices[i] < buy){
+            buy = prices[i];
+        }else{
+            let profit = prices[i] - buy;
+            maxProfit = Math.max(profit,maxProfit);
+        }
+    }
+    return maxProfit;      
+};
+
+console.log(maxProfit([7,1,5,3,6,4]));
+console.log(maxProfit([7,6,4,3,1]));
